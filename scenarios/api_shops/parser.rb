78fa9a70718b4d0017cf2b408c52ef8235a55f18
Sergey <sergeyshishkalov@gmail.com>
2014-11-04 04:25:51 +0400

    Add CSV export

--- scenarios/api_shops/parser.rb
+++ scenarios/api_shops/parser.rb
@@ -2,6 +2,7 @@ scenarios_data = []
 
 require 'nokogiri'
 require 'selenium-webdriver'
+require 'csv'
 
 class Scenario
 
@@ -22,6 +23,7 @@ class Scenario
   def setup
     @driver = Selenium::WebDriver.for :firefox
     @wait = Selenium::WebDriver::Wait.new(timeout: 5)
+    @results_file = CSV.open('parsing_results/api_shops/products_list.csv', 'w')
 
     @base_url = "http://www.apishops.com"
     @accept_next_alert = true
@@ -46,7 +48,7 @@ class Scenario
     @driver.find_element(:css, "a.modalCloseImg.simplemodal-close").click
     @driver.find_element(:id, "useTopHitsA").click
 
-    parse_page()
+    parse_page(true)
   end
 
   def element_present?(how, what)
@@ -78,7 +80,7 @@ class Scenario
 
 private
 
-  def parse_page
+  def parse_page(first_page = false)
     begin
       @wait.until { @driver.find_element(:css, '.producttable') }
     rescue => error
@@ -87,26 +89,35 @@ private
     end
 
     page = Nokogiri::HTML(@driver.page_source)
-    product_row = page.at_css('tr[alt]')
+    product_rows = page.css('tr[alt]')
 
-    product = {
-      product_id: product_row['alt'],
-      title: product_row.at_css('td.pName span').content.capitalize,
-      category_id: product_row.at_css('a.pCategory')['alt'],
-      category_title: product_row.at_css('a.pCategory').content.capitalize,
-      price: product_row.at_css('td.price')['alt'],
-      profit: product_row.at_css('td.commission')['alt'],
-      availability_level: product_row.css('.avBlock .avRect').length.to_s,
-      image_url: @base_url + product_row.at_css('td a[rel="lightbox"]')['href']
-    }
+    product_rows.each_with_index do |product_row, i|
+      product = {
+        product_id: product_row['alt'],
+        title: product_row.at_css('td.pName span').content.capitalize,
+        category_id: product_row.at_css('a.pCategory')['alt'],
+        category_title: product_row.at_css('a.pCategory').content.capitalize,
+        price: product_row.at_css('td.price')['alt'],
+        profit: product_row.at_css('td.commission')['alt'],
+        availability_level: product_row.css('.avBlock .avRect').length.to_s,
+        image_url: get_image(product_row.at_css('td a[rel="lightbox"]'))
+      }
 
-    product.values.each do |value|
-      puts value
+      @results_file << product.keys if first_page && i == 0
+      @results_file << product.values
     end
 
     if element_present?(:link, 'Следующая →')
-      # @driver.find_element(:link, 'Следующая →').click
-      # parse_page()
+      @driver.find_element(:link, 'Следующая →').click
+      parse_page()
+    end
+  end
+
+  def get_image(node)
+    unless node.nil?
+      @base_url + node['href']
+    else
+      '—'
     end
   end
 end
