42a927f188854a4d160d472e3ac3423a72a0e113
Sergey <sergeyshishkalov@gmail.com>
2014-11-29 03:20:54 +0400

    Improvements to positions keywords parser

--- scenarios/coffeedolphins/positions_keywords.rb
+++ scenarios/coffeedolphins/positions_keywords.rb
@@ -23,7 +23,7 @@ class Scenario
     @driver = Selenium::WebDriver.for :firefox
     @wait = Selenium::WebDriver::Wait.new(timeout: 5)
 
-    @base_url = "http://0.0.0.0:3000/"
+    @base_url = "http://0.0.0.0:3000"
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
   end
@@ -40,24 +40,26 @@ class Scenario
     @driver.find_element(:id, "new_user_password").send_keys "ritamargarita"
     @driver.find_element(:css, '[type="submit"]').click
 
-    @driver.find_elements(:css, '[data-view="item"]').each do |position|
-      position.find_element(:css, '[data-role="edit_item_button"]').click
-      position.find_element(:css, '.small_button.is-green[type="submit"]').click
-      wait_for { position.find_element(:css, 'span.status.is-gray') }
-    end
+    parse_page()
   end
 
 private
 
-  def wait_for(&block)
-    begin
-      yield
-    rescue => error
-      puts error.message
-      puts error.backtrace.join("\n")
+  def parse_page
+    @driver.find_elements(:css, '[data-view="item"]').each do |position|
+
+      keywords = position.find_elements(:css, '.panel_list-value .tags-item')
+      next if keywords.empty?
+
+      edit_button = position.find_element(:css, '[data-role="edit_item_button"]')
+      edit_button.click
+
+      submit_button = position.find_element(:css, '.form-actions .small_button.is-green[type="submit"]')
+      submit_button.click
+
+      @wait.until { position.find_element(:css, 'span.status.is-gray') }
     end
   end
-
 
   def element_present?(how, what)
     @driver.find_element(how, what)
